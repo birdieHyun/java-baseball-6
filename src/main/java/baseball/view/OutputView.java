83bb83c1dd8a41181abc5cd860661037bcea71ff
birdieHyun <flaehdan@gmail.com>
2023-10-19 18:10:21 +0900

    feat: 게임 시작 문구 출력 기능 개발 (#8)

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputView {
+
+    private final String START_GAME = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartGame() {
+        
+        System.out.println(START_GAME);
+    }
+}
