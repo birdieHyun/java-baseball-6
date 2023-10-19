1844bac4906e1a2b09850161849ea04f7193ba4e
birdieHyun <flaehdan@gmail.com>
2023-10-19 18:17:46 +0900

    feat: 스트라이크, 볼, 낫싱 판별 문구 개발 (#10)

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,10 @@ public class OutputView {
 
     private final String START_GAME = "숫자 야구 게임을 시작합니다.";
     private final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private final String STRIKE = "스트라이크";
+    private final String BALL = "볼";
+    private final String NOTHING = "낫싱";
+    private final String SPACE = " ";
 
     public void printStartGame() {
 
@@ -13,5 +17,31 @@ public class OutputView {
     public void printInputNumber() {
 
         System.out.print(INPUT_NUMBER);
+    }
+
+    public void printGameProgress(int strike, int ball) {
+
+        String result = convertResult(strike, ball);
+
+        System.out.println(result);
+    }
+
+    private String convertResult(int strike, int ball) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (ball > 0) {
+            stringBuilder.append(ball)
+                    .append(BALL)
+                    .append(SPACE);
+        }
+        if (strike > 0) {
+            stringBuilder.append(strike)
+                    .append(STRIKE);
+        }
+        if (strike == 0 && ball == 0) {
+            stringBuilder.append(NOTHING);
+        }
+
+        return stringBuilder.toString();
     }
 }
