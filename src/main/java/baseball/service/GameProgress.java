a3eef095642c680d2b30aa3c4130cc080fdada25
birdieHyun <flaehdan@gmail.com>
2023-10-19 18:46:58 +0900

    feat: 스트라이크 볼 비교 기능 개발 (#14)

---
+++ src/main/java/baseball/service/GameProgress.java
@@ -0,0 +1,42 @@
+package baseball.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GameProgress {
+
+    private static final String STRIKE = "strike";
+    private static final String BALL = "ball";
+
+    public Map<String, Integer> compareNumbers(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            if (isStrike(computerNumbers, userNumbers, i)) {
+                strikes++;
+                continue;
+            }
+            if (isBall(computerNumbers, userNumbers.get(i))) {
+                balls++;
+            }
+        }
+
+        Map<String, Integer> result = new HashMap<>();
+        result.put(STRIKE, strikes);
+        result.put(BALL, balls);
+
+        return result;
+    }
+
+    private boolean isStrike(List<Integer> computerNumbers, List<Integer> userNumbers, int index) {
+
+        return computerNumbers.get(index).equals(userNumbers.get(index));
+    }
+
+    private boolean isBall(List<Integer> computerNumbers, Integer userNumber) {
+
+        return computerNumbers.contains(userNumber);
+    }
+}
