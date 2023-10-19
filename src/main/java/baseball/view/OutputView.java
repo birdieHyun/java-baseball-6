e49c787a853aad5087c4af15a0f175dcdbf83ac5
birdieHyun <flaehdan@gmail.com>
2023-10-19 18:11:17 +0900

    feat: 숫자 입력 요구 문구 개발 (#9)

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,9 +3,15 @@ package baseball.view;
 public class OutputView {
 
     private final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    private final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
 
     public void printStartGame() {
-        
+
         System.out.println(START_GAME);
+    }
+
+    public void printInputNumber() {
+
+        System.out.print(INPUT_NUMBER);
     }
 }
